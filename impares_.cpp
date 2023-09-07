19f85b373416ad64b3ea994c7e1dbd3ce41cb66d
emireyes <141955808+emireyes@users.noreply.github.com>
2023-09-06 20:22:54 -0600

    Add files via upload

---
+++ impares_.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &numero);
+
+    if (numero <= 0) {
+        printf("El numero debe ser positivo.\n");
+    } else {
+        printf("Los numeros impares desde 1 hasta %d son:\n", numero);
+
+        for (int i = 1; i <= numero; i++) {
+            if (i % 2 != 0) {
+                printf("%d ", i);
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
